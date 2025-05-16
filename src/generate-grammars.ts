773abc737a3418b2f98865aa0e18e9fd96c6591b
Tomer Aberbach <tomer@aberba.ch>
2025-05-15 23:06:53 -0400

    fix: more robust to unclosed lang constructs (#2)

--- src/generate-grammars.ts
+++ src/generate-grammars.ts
@@ -32,6 +32,26 @@ export const generateBasicGrammar = (): Record<string, unknown> => ({
   ),
   patterns: LANGUAGES.map(generatePattern),
   scopeName: BASIC_GRAMMAR_SCOPE_NAME,
+  repository: Object.assign(
+    {},
+    ...LANGUAGES.flatMap(language =>
+      language.grammars.flatMap((grammar, index) => {
+        grammar = bailOnBacktick(
+          qualifyLocalIncludes(grammar, `${language.name}-${index}`),
+        )
+        let repository: unknown
+        if (
+          typeof grammar === `object` &&
+          grammar !== null &&
+          `repository` in grammar
+        ) {
+          ;({ repository } = grammar)
+          delete grammar.repository
+        }
+        return [{ [`${language.name}-${index}`]: grammar }, repository]
+      }),
+    ),
+  ),
 })
 export const BASIC_GRAMMAR_SCOPE_NAME = `inline.tagged-template-languages`
 export const BASIC_GRAMMAR_PATH = `./dist/basic-grammar.json`
@@ -56,14 +76,104 @@ const generatePattern = (language: Language) => ({
     1: { name: `entity.name.function.tagged-template.js` },
     2: { name: `punctuation.definition.string.template.begin.js` },
   },
-  end: `\``,
+  end: `(?<!\\\\)\``,
   endCaptures: {
     0: { name: `punctuation.definition.string.template.end.js` },
   },
-  patterns: [
-    ...Array.from(language.sources, source => ({ include: source })),
-    // When a language grammar is not installed, insert a catch-all pattern so that we still match all the
-    // inner text but don't highlight it.
-    { match: `.` },
-  ],
+  patterns: language.grammars.map((_, index) => ({
+    include: `#${language.name}-${index}`,
+  })),
 })
+
+/**
+ * Prefixes all local includes with the given qualifier.
+ *
+ * This is necessary because we inject multiple grammars into the same file, but
+ * `repository` must be at the top level of the grammar. So the repository of
+ * each grammar must be prefixed with the name of the grammar to avoid
+ * collisions.
+ */
+const qualifyLocalIncludes = (grammar: unknown, qualifier: string): unknown => {
+  if (typeof grammar !== `object` || grammar === null) {
+    return grammar
+  }
+
+  if (Array.isArray(grammar)) {
+    return grammar.map(value => qualifyLocalIncludes(value, qualifier))
+  }
+
+  const qualifiedGrammar = Object.fromEntries(
+    Object.entries(grammar).map(([key, value]) => [
+      key,
+      qualifyLocalIncludes(value, qualifier),
+    ]),
+  )
+
+  if (
+    `repository` in qualifiedGrammar &&
+    typeof qualifiedGrammar.repository === `object` &&
+    qualifiedGrammar.repository !== null
+  ) {
+    qualifiedGrammar.repository = Object.fromEntries(
+      Object.entries(qualifiedGrammar.repository).map(([key, value]) => [
+        `${qualifier}-${key}`,
+        value,
+      ]),
+    )
+  }
+
+  if (
+    `include` in qualifiedGrammar &&
+    typeof qualifiedGrammar.include === `string`
+  ) {
+    if (qualifiedGrammar.include.startsWith(`#`)) {
+      qualifiedGrammar.include = `#${qualifier}-${qualifiedGrammar.include.slice(1)}`
+    } else if (qualifiedGrammar.include === `$self`) {
+      qualifiedGrammar.include = `#${qualifier}`
+    }
+  }
+
+  return qualifiedGrammar
+}
+
+/**
+ * Force the grammar to end on backticks so that it doesn't spill out of the
+ * template literal for unclosed language constructs.
+ */
+const bailOnBacktick = (grammar: unknown): unknown => {
+  if (typeof grammar !== `object` || grammar === null) {
+    return grammar
+  }
+
+  const grammarCopy = { ...grammar } as Record<string, unknown>
+
+  if (`end` in grammarCopy && typeof grammarCopy.end === `string`) {
+    grammarCopy.end = `${grammarCopy.end}|(?<!\\\\)(?=\`)`
+  }
+
+  if (`patterns` in grammarCopy && Array.isArray(grammarCopy.patterns)) {
+    grammarCopy.patterns = grammarCopy.patterns.map(bailOnBacktick)
+  }
+
+  for (const key of [
+    `repository`,
+    `captures`,
+    `beginCaptures`,
+    `endCaptures`,
+  ]) {
+    if (
+      key in grammarCopy &&
+      typeof grammarCopy[key] === `object` &&
+      grammarCopy[key] !== null
+    ) {
+      grammarCopy[key] = Object.fromEntries(
+        Object.entries(grammarCopy[key]).map(([key, value]) => [
+          key,
+          bailOnBacktick(value),
+        ]),
+      )
+    }
+  }
+
+  return grammarCopy
+}
